6c86527af89c7532e1b0640ce6df131da3486a83
zhaojingyu00 <2819361152@qq.com>
2019-12-04 14:27:22 +0800

    a

--- menghan/后端页面+数据库+读取+删除/Wuchong/app.js
+++ menghan/后端页面+数据库+读取+删除/Wuchong/app.js
@@ -1,8 +1,27 @@
-var createError = require('http-errors');
 var express = require('express');
+var router = express.Router();
+var data=require('./data.json');
+var List=data.chapterList;
+var mysql=require('mysql');
+var dbconfig = require('./config/dbconfig.json');
+var con = mysql.createConnection(dbconfig);
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
+con.connect();
+
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,9 +62,9 @@ app.use('/dongtai', dongtaiRouter);
 app.use('/system', systemRouter);
 app.use('/userguanli', userguanRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 
 // error handler
@@ -58,6 +77,86 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+app.get('/active',function(err,res){
+  con.query('select * from active',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+}) ;
+
+app.get('/clockin',function(err,res){
+  con.query('select * from clockin',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+});
+
+app.get('/dynamic',function(err,res){
+  con.query('select * from dynamic',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+});
+
+app.get('/manager',function(err,res){
+  con.query('select * from manager',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+});
+
+app.get('/petinfo',function(err,res){
+  con.query('select * from petinfo',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+});
+
+app.get('/signup',function(err,res){
+  con.query('select * from signup',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+});
+
+app.get('/userinfo',function(err,res){
+  con.query('select * from userinfo',function(err,result){
+      if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+      }
+      res.json(result); 
+  }); 
+}) 
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("地址为 http://%s:%s", host, port);
+})
 
 
 module.exports = app;
