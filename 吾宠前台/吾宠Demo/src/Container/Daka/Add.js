b507c63514ff43b0e8324483aeed8279c4a1b8e7
siyuannnnn-hsy <1587935941@qq.com>
2019-12-11 15:18:29 +0800

    daka

--- 吾宠前台/吾宠Demo/src/Container/Daka/Add.js
+++ 吾宠前台/吾宠Demo/src/Container/Daka/Add.js
@@ -21,48 +21,81 @@ function getBase64(img, callback) {
   }
 
 export default class Add extends Component {
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state={
-          clockName:""
-        }
+          clockName:"",
+          clockNum:"",
+          clockTime:"",
+          userId:"2",
+          clockImg:"",
+          clockCycle:"",
+          
+      }
     }
-    nameChange=(e)=>{
 
-      this.setState({
-        clockName:e.target.value
-      })
+
+    clockNameChange=(e)=>{
+        console.log(e.target.value);
+        this.setState({
+            clockName:e.target.value
+        })
     }
+    clockNumChange=(e)=>{
+        console.log(e.target.value);
+        this.setState({
+          clockNum:e.target.value
+        })
+    }
+    clockTimeChange=(e)=>{
+        console.log(e.target.value);
+        this.setState({
+          clockTime:e.target.value
+        })
+    }
+    // clockImgChange=(e)=>{
+    //     console.log(e.target.value);
+    //     this.setState({
+    //       clockImg:e.target.value
+    //     })
+    // }
+    // clockCycleChange=(e)=>{
+    //     console.log(e.target.value);
+    //     this.setState({
+    //       clockCycle:e.target.value
+    //     })
+    // }
     register=()=>{
-      // console.log(this.state.userName);
-      const registerValue = {"clockName":this.state.clockName}
-     
-      if(this.state.clockName!=""){
-         fetch('/clockin', {
-               method: "POST",
-               headers: {
-                   "Content-type":"application/json;charset=utf-8",
-               },
-               body:JSON.stringify(registerValue) ,
-          }).then( res => res.text())
-            .then( data => {
-                console.log(data);
-              //   if(data.success){
-              //       alert('register OK');
-              //   }
-            });
-          }
-   }
-      state = {
-        value: null,
-      };
-      onChange = (value) => {
-        console.log(value);
-        this.setState({ value });
-      };
-      onValueChange = (...args) => {
-        console.log(args);
-      };
+        console.log(this.state.clockName);
+        const registerValue = {"clockName":this.state.clockName,"clockNum": this.state.clockNum,"clockTime": this.state.clockTime,"userId":this.state.userId,"clockImg":this.state.clockImg,"clockCycle":this.state.clockCycle}
+      
+        // if(this.state.clockName!=""&&this.state.clockNum!=""&&this.state.clockTime!=""&&this.state.clockImg!=""&&this.state.clockCycle!=""){
+          fetch('/clockin', {
+                method: "POST",
+                headers: {
+                    "Content-type":"application/json;charset=utf-8",
+                },
+                body:JSON.stringify(registerValue) ,
+            }).then( res => res.text())
+              .then( data => {
+                  console.log(data);
+              });
+            // }
+        this.props.history.push('/clockin');
+    }
+
+      
+      // state = {
+      //   value: '',
+      //   loading: false,
+      // };
+      // onChange = (value) => {
+      //   console.log(value);
+      //   this.setState({ value });
+      // };
+      // onValueChange = (...args) => {
+      //   console.log(args);
+      // };
     
     daka = (e) => {
         var div1=document.getElementById('daka');
@@ -76,6 +109,9 @@ export default class Add extends Component {
         var every = document.getElementById('every');
         span.innerHTML='设置'+e.target.innerHTML+'打卡次数：';
         every.innerHTML=e.target.innerHTML;
+        this.setState({
+          clockCycle:every.innerHTML
+        })
     }
     
     add = () => {
@@ -83,7 +119,7 @@ export default class Add extends Component {
         shezhi.style.display='block'
     }
 
-    clockin = () => {
+    clockin1 = () => {
         this.props.history.push('/clockin');
     }
 
@@ -93,22 +129,25 @@ export default class Add extends Component {
       };
     
       handleChange = info => {
+        console.log(info.file.name)
         if (info.file.status === 'uploading') {
           this.setState({ loading: true });
           return;
         }
         if (info.file.status === 'done') {
+          let clockImg="https://liwenroul.github.io/Wuchong/img/avatar/"+info.file.name;
           // Get this url from response in real world.
           getBase64(info.file.originFileObj, imageUrl =>
             this.setState({
               imageUrl,
               loading: false,
+              clockImg:clockImg
             }),
           );
         }
       };
 
-      
+    
 
     render() {
         const uploadButton = (
@@ -123,8 +162,8 @@ export default class Add extends Component {
                 <div>
                     <NavBar
                             style={{backgroundColor:'rgb(29,174,169)',color:'#000'}}
-                            leftContent={<i style={{fontSize:22,color:'white'}} className='iconfont icon-back' onClick={this.clockin}></i>}
-                            rightContent={<i style={{fontSize:22,color:'white'}} className='iconfont icon-duihao' onClick={this.register}></i> }
+                            leftContent={<i style={{fontSize:22,color:'white'}} className='iconfont icon-back' onClick={this.clockin1}></i>}
+                            rightContent={<i style={{fontSize:22,color:'white'}} className='iconfont icon-duihao'  onClick={this.register}></i> }
                         >互动</NavBar>
                     <div className="add">
                         <div className='to'>
@@ -138,10 +177,10 @@ export default class Add extends Component {
                                 onChange={this.handleChange}
                                 // beforeUpload={this.handleBeforeUpload}
                             >
-                                {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '90px',height:'90px',borderRadius:'50%',marginLeft:'-8px',marginTop:'-8px'}} /> : uploadButton}
+                                {imageUrl ? <img src={imageUrl} onChange={this.clockImgChange} id='clockImg' alt="avatar" style={{ width: '90px',height:'90px',borderRadius:'50%',marginLeft:'-8px',marginTop:'-8px'}} /> : uploadButton}
                             </Upload>
                         </div>
-                        <input onChange={this.nameChange} type="text" placeholder="给互动命名" style={{textAlign:'center',marginLeft:'110px',marginTop:'20px',borderRadius:'5px',background:'#eee'}}/>
+                        <input type="text" id='clockName' onChange={this.clockNameChange}  placeholder="给互动命名" style={{textAlign:'center',marginLeft:'110px',marginTop:'20px',borderRadius:'5px',background:'#eee'}}/>
                     </div>
                     <div className="add2">
                         <span style={{fontSize:15,marginTop:'30px',marginLeft:'5px',float:'left',fontWeight:'bolder'}} id='span'>设置每日打卡次数：</span>
@@ -150,21 +189,24 @@ export default class Add extends Component {
                         <div className="daka2" id='daka3' onClick={this.daka}>每月</div>
                         <div className="add3">
                             <span style={{fontWeight:'bolder',marginLeft:'5px'}} id='every'>每日</span>
-                            <input type='text' placeholder="1" style={{height:'30px',width:'40px',marginLeft:'5px',borderRadius:'10px',textAlign:'center',fontWeight:'bolder',lineHeight:'40px'}}/>
+                            <input type='text' onChange={this.clockNumChange} id='clockNum' placeholder="1" style={{height:'30px',width:'40px',marginLeft:'5px',borderRadius:'10px',textAlign:'center',fontWeight:'bolder',lineHeight:'40px'}}/>
                             <span style={{fontWeight:'bolder',marginLeft:'5px'}}>次</span>
                         </div>
                         <span style={{fontSize:15,marginTop:'20px',marginLeft:'5px',float:'left',fontWeight:'bolder'}}>设置提醒时间：</span>
                     </div>
-                    <div className="jiahao" onClick={this.add}>+</div>
-                    <div className='shezhi' id='shezhi'>
+                    <div >
+                      <input type='text' onChange={this.clockTimeChange} id='clockTime' className='time' placeholder='请输入时间 例如 08:00'/>
+                    </div>
+                    {/* <div className="jiahao" onClick={this.add}>+</div>
+                    <div className='shezhi' id='shezhi' >
                         <DatePickerView
                             value={this.state.value}
-                            onChange={this.onChange}
+                            onChange={this.clockTimeChange}
                             onValueChange={this.onValueChange}
                             mode='time'
                             itemStyle={{color:'rgb(29,174,169)',fontSize:20}}
                         />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         )
--- 吾宠后台/Wuchong/app.js
+++ 吾宠后台/Wuchong/app.js
@@ -156,7 +156,7 @@ app.get('/clockin',function(err,res){
   }); 
 });
 
-app.get('/dynamic',jsonParser,function(err,res){
+app.get('/dynamic',function(err,res){
   con.query('select * from dynamic',function(err,result){
       if(err){
           console.log('[SELECT ERROR] - ', err.message);
@@ -202,7 +202,7 @@ app.get('/signup',function(err,res){
           console.log('[SELECT ERROR] - ', err.message);
           return;
       }
-      res.json(result);
+      res.json(result); 
   }); 
 });
 
@@ -249,6 +249,28 @@ app.post('/userinfo1',(req,res)=>{
   })
 })
 
+app.post('/clockin',(req,res)=>{
+  let data=req.body;
+  console.log(data);
+  let insertData = {
+    clockId:"wuchong"+parseInt(Math.random()*1000000),
+    clockName:data.clockName,
+    clockNum:data.clockNum,
+    clockTime:data.clockTime,
+    userId:data.userId,
+    clockImg:data.clockImg,
+    clockCycle:data.clockCycle,
+    
+  }
+  con.query('insert into clockin(clockId,clockName,clockNum,clockTime,userId,clockImg,clockCycle) values(?,?,?,?,?,?,?)',[insertData.clockId,insertData.clockName,insertData.clockNum,insertData.clockTime,insertData.userId,insertData.clockImg,insertData.clockCycle],function(err,result){
+    if(err){
+          console.log(err);
+      }
+        console.log(result);
+        res.json(result); 
+  })
+})
+
 app.post('/petinfo',(req,res)=>{
   let data=req.body;
   console.log(data);
@@ -257,7 +279,7 @@ app.post('/petinfo',(req,res)=>{
     petName:data.petName,
     petSex:data.petSex,
     petAge:data.petAge,
-    userId:data.userId,
+    userId:"1",
     petImg:""
   }
   con.query('insert into petinfo(petId,petName,petSex,petAge,userId,petImg) values(?,?,?,?,?,?)',[insertData.petId,insertData.petName,insertData.petSex,insertData.petAge,insertData.userId,insertData.petImg],function(err,result){
@@ -272,6 +294,9 @@ app.post('/petinfo',(req,res)=>{
 app.post('/denglu',(req,res)=>{
   let data=req.body;
   console.log(data);
+  // let insertData = {
+  //   userId:data.userId,
+  // }
   con.query("select * from denglu",function(err,result){
     if(err){
       console.log(err);
@@ -284,7 +309,8 @@ app.post('/denglu',(req,res)=>{
             }else{
               console.log(result);
               res.json(result);
-            }       
+            }
+               
         })
       }
       else{
@@ -301,70 +327,6 @@ app.post('/denglu',(req,res)=>{
     }
   })
   
-})
-
-app.post('/dynamic',(req,res)=>{
-  let data=req.body;
-  console.log(data);
-  let insertData = {
-    dynamicId:"dynamic"+parseInt(Math.random()*1000000),
-    dynamicImg:data.dynamicImg,
-    dynamicContent:data.dynamicContent,
-    likeNum:1,
-    userId:data.userid
-  }
-  con.query('insert into dynamic(dynamicId,dynamicImg,dynamicContent,likeNum,userid) values(?,?,?,?,?)',[insertData.dynamicId,insertData.dynamicImg,insertData.dynamicContent,insertData.likeNum,insertData.userId],function(err,result){
-    if(err){
-          console.log(err);
-      }
-        console.log(result);
-        res.json(result); 
-  })
-})
-app.post('/signup1',(req,res)=>{
-  let data=req.body;
-  console.log(data);
-  let insertData = {
-    signId:"active"+parseInt(Math.random()*1000000),
-    signName:data.signName,
-    signTel:data.signTel,
-    spetName:data.spetName,
-    spetAge:data.spetAge,
-    spetKind:data.spetKind,
-    activeId:data.activeId,
-    userId:data.userId
-  }
-  con.query('insert into signup(signId,signName,signTel,spetName,spetAge,spetKind,activeId,userId) values(?,?,?,?,?,?,?,?)'
-  ,[insertData.signId,insertData.signName,insertData.signTel,insertData.spetName,insertData.spetAge
-  ,insertData.spetKind,insertData.activeId,insertData.userId],function(err,result){
-    if(err){
-          console.log(err);
-      }
-        console.log(result);
-        res.json(result); 
-  })
-})
-app.post('/userinfo2',(req,res)=>{
-  let data=req.body;
-  console.log(data);
-  con.query('update userinfo set userAvatar=? where userId=?',[data.userAvatar,data.userId],function(err,result){
-    if(err){
-          console.log(err);
-      }
-        console.log(result);
-        res.json(result); 
-  })
-})
-app.post('/userinfo3',(req,res)=>{
-  let data=req.body;
-  console.log(data);
-  con.query('update userinfo set userName=? where userId=?',[data.userName,data.userId],function(err,result){
-    if(err){
-          console.log(err);
-      }
-        console.log(result);
-        res.json(result); 
-  })
 })
 
 var server = app.listen(8081, function () {
